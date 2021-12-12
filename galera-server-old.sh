b93a4c747473a4580b4edf2d24e17154b3b56908
Zoe Song <zoe@Zoes-MacBook-Pro.local>
2021-12-12 11:37:11 -0600

    add old

--- galera-server-old.sh
+++ galera-server-old.sh
@@ -31,8 +31,8 @@ elif [ $node -eq 3 ];then
 fi
 
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu xenial main'
-sudo apt update
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu xenial main'
+sudo apt-get update
 sudo apt install mariadb-server
 
 sudo mysql -uroot -e "set password = password("123456"); quit;"
