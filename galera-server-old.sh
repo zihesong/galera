3a36e0b7ff385e371cad4af3205adcb7d2ee936a
Zoe Song <zoe@Zoes-MacBook-Pro.local>
2021-12-12 11:35:50 -0600

    add old

--- galera-server-old.sh
+++ galera-server-old.sh
@@ -31,7 +31,7 @@ elif [ $node -eq 3 ];then
 fi
 
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu bionic main'
+sudo add-apt-repository 'deb [arch=amd64] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu xenial main'
 sudo apt update
 sudo apt install mariadb-server
 
